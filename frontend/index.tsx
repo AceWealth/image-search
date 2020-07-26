fb68915f6976bcb06d66fa64c04546efc139571c
Ashwanth Kumar <ashwanth.kumar@avalara.com>
2020-07-26 14:31:37 +0530

    Putting them all together

---
+++ frontend/index.tsx
@@ -0,0 +1,74 @@
+import {
+    Text,
+    initializeBlock,
+    useViewport,
+    useSettingsButton,
+    Box,
+} from '@airtable/blocks/ui';
+import {viewport, runInfo} from '@airtable/blocks';
+
+import React, { useState, useEffect } from 'react';
+import { SettingsView } from './SettingsView';
+import { SearchPage } from './SearchPage';
+import { SearchResultsView } from './SearchResultsView';
+import { ReviewSelection } from './ReviewSelection';
+import { ThankYou } from './ThankYouView';
+import { useSettings } from './settings';
+
+type AppState = {
+    index: number,
+    state: object,
+}
+
+viewport.addMinSize({
+    // height: 800,
+    width: 1501,
+});
+
+viewport.addMaxFullscreenSize({
+    // height: 800,
+    width: 1501,
+});
+
+function SearchAndImportImagesBlock() {
+    const viewport = useViewport();
+    const { isValid, message, settings } = useSettings();
+    const [isSettingsVisible, setIsSettingsVisible] = useState(false);
+    useSettingsButton(() => {
+        setIsSettingsVisible(!isSettingsVisible);
+    });
+
+    // Open the SettingsForm whenever the settings are not valid
+    useEffect(() => {
+        if (!isValid) {
+            setIsSettingsVisible(true);
+        }
+    }, [isValid]);
+
+    const [appState, setAppState] = useState<AppState>({ index: 1, state: {} });
+
+    if (!isValid || isSettingsVisible) {
+        return (<SettingsView appState={appState} setAppState={setAppState} setIsSettingsVisible={setIsSettingsVisible}/>);
+    }
+
+    switch (appState.index) {
+        case 1:
+            return (<SearchPage appState={appState} setAppState={setAppState} />);
+        case 2:
+            return (<SearchResultsView appState={appState} setAppState={setAppState} />);
+        case 3:
+            return (<ReviewSelection appState={appState} setAppState={setAppState} />);
+        case 4:
+            return (<ThankYou appState={appState} setAppState={setAppState} />);
+        default:
+            return (<NotFoundPage appState={appState} />);
+    }
+}
+
+function NotFoundPage({ appState }) {
+    return (
+        <Text>Invalid App State Index: {appState.index}, State: {JSON.stringify(appState.state)}</Text>
+    );
+}
+
+initializeBlock(() => <SearchAndImportImagesBlock />);
